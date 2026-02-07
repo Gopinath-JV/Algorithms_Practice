1f21fa95823b560c097920fcfba16b2af8210979
Gopinath JV <gopinathmlengineer@gmail.com>
2026-02-07 22:58:16 +0530

    Remove wrong file

--- 01_Basic_Logic_and_Control_Flow/leap_year_or_not.py
+++
@@ -1,9 +0,0 @@
-year = (int(input("Enter the year:")))
-if year <= 0:
-    print("the entered year", year, "is invalid")
-elif (year % 400 == 0):
-    print("The entered year", year, "is a leap year")
-elif (year % 4 == 0) and (year % 100 != 0):
-    print("The entered year", year, "is a leap year")
-else:
-    print("the entered year", year, "is not a leap year")
